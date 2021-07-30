9a6eba3331cd436206acfc01d850da51d1c208f7
Esther Wavinya <estherwavinya80@gmail.com>
2021-07-30 16:04:51 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,48 @@
+function toggleTheme() {
+    const body = document.querySelector("body");
+    const icon = document.getElementById("icon");
+    if (body.className === 'dark') {
+        body.className = 'light'
+        icon.className = 'fa fa-moon-o'
+    }
+    else {
+        body.className = 'dark'
+        icon.className = 'fa fa-sun-o'
+    }
+}
+
+const burger = document.getElementById('burger');
+const ul = document.querySelector('nav ul');
+const homeAnchor = document.getElementById('home-anchor');
+const skillAnchor = document.getElementById('skill-anchor');
+const eduAnchor = document.getElementById('edu-anchor');
+const expAnchor = document.getElementById('exp-anchor');
+const projAnchor = document.getElementById('proj-anchor');
+const openAnchor = document.getElementById('open-anchor');
+const contaAnchor = document.getElementById('conta-anchor');
+
+burger.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+
+homeAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+skillAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+eduAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+expAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+projAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+openAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
+contaAnchor.addEventListener('click', () => {
+    ul.classList.toggle('show');
+})
